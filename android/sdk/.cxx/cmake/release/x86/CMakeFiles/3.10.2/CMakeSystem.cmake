8b00f75b075d290363dccc17b0cc7a183af4232b
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2021-08-12 18:59:30 +0900

    commit

--- android/sdk/.cxx/cmake/release/x86/CMakeFiles/3.10.2/CMakeSystem.cmake
+++ android/sdk/.cxx/cmake/release/x86/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_HOST_SYSTEM "Windows-10.0.18362")
-set(CMAKE_HOST_SYSTEM_NAME "Windows")
-set(CMAKE_HOST_SYSTEM_VERSION "10.0.18362")
-set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+set(CMAKE_HOST_SYSTEM "Darwin-20.3.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "20.3.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("C:/Users/hwang/AppData/Local/Android/Sdk/ndk/21.0.6113669/build/cmake/android.toolchain.cmake")
+include("/Users/lyk/Library/Android/sdk/ndk/21.0.6113669/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
